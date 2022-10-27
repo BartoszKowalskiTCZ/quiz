5dec22c1a6ad9c9d278325f23969032720f05403
BartoszKowalskiTCZ <bartek.kowalski20052004@gmail.com>
2022-10-27 08:53:13 +0200

    xx

--- pytaniaiodpowiedzi.sql
+++ pytaniaiodpowiedzi.sql
@@ -1,80 +1,80 @@
 P Y T A N I A 
 1pytanie
-INSERT INTO `questions`(`description`) VALUES (`12+12=?`):24
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma szary olowek?`):szary
 2pytanie
-INSERT INTO `questions`(`description`) VALUES (`15+15=?`):30
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma niebieski dlugopis?`):niebieski
 3pytanie
-INSERT INTO `questions`(`description`) VALUES (`20+20=?`):40
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma zolty telefon?`):zolty
 4pytanie
-INSERT INTO `questions`(`description`) VALUES (`40+40=?`):80
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma czarna myszka?`):czarny
 5pytanie
-INSERT INTO `questions`(`description`) VALUES (`50+50=?`):100
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma czerwony monitor?`):czerwony
 6pytanie
-INSERT INTO `questions`(`description`) VALUES (`70+70=?`):140
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma pomaranczowa sciana?`):pomaranczowy
 7pytanie
-INSERT INTO `questions`(`description`) VALUES (`90+90=?`):180
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma brazowy kon?`):brazowy
 8pytanie
-INSERT INTO `questions`(`description`) VALUES (`210+210=?`):420
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma niebieski obraz?`):niebieski
 9pytanie
-INSERT INTO `questions`(`description`) VALUES (`250+250=?`):500
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma niebieski stol?`):niebieski
 10pytanie
-INSERT INTO `questions`(`description`) VALUES (`700+300=?`):1000
+INSERT INTO `questions`(`description`) VALUES (`jaki kolor ma bialy sufit?`):bialy
 
 
  O D P O W I E D Z I 
 odpowiedz1-5odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (24,1,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (25,1,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (26,1,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (27,1,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (28,1,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (szary,1,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (niebieski,1,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zolty,1,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,1,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,1,0):
 ,1,0):
 odpowiedz2-3odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (30,2,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (31,2,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (32,2,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (niebieski,2,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,2,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (brazowy,2,0):
 
 odpowiedz3-3odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (40,3,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (41,3,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (42,3,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zolty,3,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,3,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,3,0):
 
 odpowiedz4-4odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (80,4,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (81,4,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (82,4,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (82,4,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (czarny,4,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,4,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,4,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (czerwony,4,0):
 
 odpowiedz5-4odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (100,5,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (101,5,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (102,5,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (103,5,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (czerwony,5,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (granatowy,5,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (niebieksi,5,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,5,0):
 
 odpowiedz6-4odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (140,6,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (141,6,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (142,6,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (143,6,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (pomaranczowy,6,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (fioletowy,6,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,6,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (niebieski,6,0):
 
 odpowiedz7-4odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (180,7,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (181,7,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (182,7,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (183,7,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (brazowy,7,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,7,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zolty,7,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (granatowy,7,0):
 
 odpowiedz8-5odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (420,8,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (421,8,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (423,8,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (424,8,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (425,8,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (niebieski,8,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,8,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zolty,8,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (fioletowy,8,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (granatowy,8,0):
 
 odpowiedz9-3odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (500,9,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (501,9,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (502,9,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (niebieski,9,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (pomaranczowy,9,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,9,0):
 odpowiedz10-3odpowiedzi
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (1001,10,1):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (1001,10,0):
-INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (1002,10,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (bialy,10,1):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zolty,10,0):
+INSERT INTO `answers`(`description`,`questions_id`, `is_right`) VALUES (zielony,10,0):

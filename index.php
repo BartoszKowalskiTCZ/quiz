aed2ee2ffa488010c61b0b29bb5787a92efe2d2a
BartoszKowalskiTCZ <bartek.kowalski20052004@gmail.com>
2022-11-02 15:10:16 +0100

    x

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 
 <html>
 	<head>
-        <meta=charset="utf-8">
+        <meta charset="utf-8">
         <title>quiz</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-    
 <header>
-    <a href="index.html"></a>
+    <a href="index.html">quiz</a>
         </header>
-        
-        <div class="mainpane">
-<form method="post">
+        <form method="post">
         <?php
 		$con = new mysqli("127.0.0.1", "root", " ", "quiz");
-        $res = $con->querry("SELECT * FROM questions");
-        $rows = $res->fetch_all(MYSQLI_ASSOC)
+        $res = $con->query("SELECT id, description FROM questions");
+        $rows = $res->fetch_all(MYSQLI_ASSOC);
+            $
         for($i=0, $i<count($rows);$i++){
-            echo $rows[$i][" "];
+            echo 
+            echo $rows[$i]["desc"];
             $res2 = %con->querry("SELECT ");
             $rows2 = $res2->fetch_all(MYSQLI_ASSOC);
-            for($j=0;$count
+            for($j=0;$count($rows2);$j++{
+                echo
+                
+            };            
+                
+                ?>
             
             
             
+            
+            //
             print_r($rows);
             $rows[$i]
         }

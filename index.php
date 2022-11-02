4a8533055541dabc8fb7ced72ed2b545c045f570
BartoszKowalskiTCZ <bartek.kowalski20052004@gmail.com>
2022-11-02 16:02:05 +0100

    nowezm

--- index.php
+++ index.php
@@ -3,38 +3,43 @@
 	<head>
         <meta charset="utf-8">
         <title>quiz</title>
-        <link rel="stylesheet" href="style.css">
+        <style type="text/css">
+   body {
+   background-color:beige;
+        }
+        </style>
     </head>
     <body>
-<header>
-    <a href="index.html">quiz</a>
-        </header>
-        <form method="post">
         <?php
-		$con = new mysqli("127.0.0.1", "root", " ", "quiz");
-        $res = $con->query("SELECT id, description FROM questions");
+		$con = new mysqli("127.0.0.1", "root", " ", "mydb");
+        $pyt = $con->query("SELECT id, description FROM questions");
+        $odp = $con -> query("SELECT description, questions_id,is_right FROM answers");
+        echo '<form method="POST" action="wynik.php">';
         $rows = $res->fetch_all(MYSQLI_ASSOC);
-            $
-        for($i=0, $i<count($rows);$i++){
-            echo 
-            echo $rows[$i]["desc"];
-            $res2 = %con->querry("SELECT ");
+            
+           for($i=0;$i<count($rows);$i++) 
+        {
+            echo 'div class="  ">pytanie '$i: '
+            
+            
+            //////
+            echo $rows[$i]["description"];
+            $res2 = %con->query("SELECT * FROM answers WHERE");
             $rows2 = $res2->fetch_all(MYSQLI_ASSOC);
-            for($j=0;$count($rows2);$j++{
-                echo
-                
-            };            
-                
+            for($j=0;$j<count($rows2);$j++){
+                echo 'input type="checkbox">'.$rows2[$j]["description"].
+            };
+           echo '<input type>="submit"/>';
                 ?>
             
             
+        </form>
+    </body>
+</html>
             
             
-            //
-            print_r($rows);
-            $rows[$i]
-        }
-        
+            
+           
         
         
         

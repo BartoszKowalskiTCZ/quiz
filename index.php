b1ba41861ea46a40e28fa9d68307da2f0d7cb4c0
BartoszKowalskiTCZ <113582324+BartoszKowalskiTCZ@users.noreply.github.com>
2022-11-02 18:31:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,39 +10,27 @@
         </style>
     </head>
     <body>
-        <?php
-		$con = new mysqli("127.0.0.1", "root", " ", "mydb");
-        $pyt = $con->query("SELECT id, description FROM questions");
-        $odp = $con -> query("SELECT description, questions_id,is_right FROM answers");
-        echo '<form method="POST" action="wynik.php">';
-        $rows = $res->fetch_all(MYSQLI_ASSOC);
-            
-           for($i=0;$i<count($rows);$i++) 
-        {
-            echo 'div class="  ">pytanie '$i: '
-            
-            
-            //////
-            echo $rows[$i]["description"];
-            $res2 = %con->query("SELECT * FROM answers WHERE");
-            $rows2 = $res2->fetch_all(MYSQLI_ASSOC);
-            for($j=0;$j<count($rows2);$j++){
-                echo 'input type="checkbox">'.$rows2[$j]["description"].
-            };
-           echo '<input type>="submit"/>';
-                ?>
-            
-            
-        </form>
-    </body>
-</html>
-            
-            
-            
-           
-        
-        
-        
+        <?php	
+$con = new mysqli("127.0.0.1","root"," ","mydb"); 
+$res = $con->query("SELECT id, description FROM questions");
+ $rows = $res->fetch_all(MYSQLI_ASSOC); 
+<form method="POST" action="wynik.php">;
+for($i=0;$i<count($rows);$i++){
+ echo '<b>'.$rows[$i]["description"].'</b>'.'</br>'; 
+
+
+
+$res2 = $con->query("SELECT description, questions_id FROM answers WHERE questions_id = ".$rows[$i]['id']);
+$rows2 = $res2->fetch_all(MYSQLI_ASSOC);
+ for($j=0;$j<count($rows2);$j++){ 
+echo $rows2[$j]["description"].<input type="checkbox" name="'.$i.'.$j.'" value="1"/>'.'<br>';
+        }; 
+ }; 
+?>
+<input type="submit">
+</form>
+</body>
+ </html>
         
         
         
